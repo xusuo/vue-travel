06cb1bf0d759dc8e9822432666b0a2b08dd910dd
xusuo <1901532726@qq.com>
2018-10-24 21:22:04 +0800

    c

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,11 +2,12 @@
 <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :cities="cities" :hotCities="hotCities"></city-list>
+    <city-alphabet :cities="cities"></city-alphabet>
 </div>
 </template>
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
@@ -18,6 +19,28 @@ export default {
         CitySearch,
         CityList,
         CityAlphabet
+    },
+    methods:{
+        getCityImfo(){
+            axios.get('/api/city.json').then(this.getInfoSucc)
+        },
+        getInfoSucc(res){
+            res = res.data
+            if(res.ret && res.data){
+                const data = res.data
+                this.cities = data.cities
+                this.hotCities = data.hotCities
+            }
+        }
+    },
+    data(){
+        return{
+            cities:{},
+            hotCities:[]
+        }
+    },
+    mounted(){
+        this.getCityImfo()
     }
 }
 </script>
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -12,62 +12,15 @@
             <div class="area">
                 <div class="title border-topbottom">热门城市</div>
                 <div class="button-list">
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
+                    <div class="button-wrapper" v-for="item of hotCities" :key="item.id">
+                        <div class="button">{{item.name}}</div>
+                    </div>                    
                 </div>
             </div>
-            <div class="area">
-                <div class="title border-topbottom">B</div>
+            <div class="area" v-for="(item ,key) of cities" :key="key">
+                <div class="title border-topbottom">{{key}}</div>
                 <div class="item-list">
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
-                    <div class="item border-bottom">徐州</div>
+                    <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">{{innerItem.name}}</div>                    
                 </div>
             </div>
         </div>
@@ -77,9 +30,12 @@
 import BetterScroll from 'better-scroll'
 export default {
   name: "CityList",
+  props:{
+      hotCities:Array,
+      cities:Object
+  },
   mounted(){
       this.scroll = new BetterScroll(this.$refs.wrapper)
-
   }
 };
 </script>
